50ba24265f3a529c129df6082b96464861e74c3f
shreyansh <viragi311@gmail.com>
2019-09-04 15:36:08 -0700

    scema boiler plates

---
+++ schema/schema.js
@@ -0,0 +1,13 @@
+const graphql = require("graphql");
+
+const {GraphQLObjectType,GraphQLString} = graphql;
+
+const bookType = new GraphQLObjectType({
+    name:"Book",
+    fields:()=>({
+        id: {type:GraphQLString},
+        name:{type: GraphQLString},
+        genre:{type:GraphQLString}
+    })
+
+})
