6625c1bd9b510ec7e71cefbbc7a3bea50ea17a34
shreyansh <viragi311@gmail.com>
2019-09-05 15:22:13 -0700

    making grapiql query

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-
+const schema = require("./schema/schema");
 const app = express();
 
-app.use('/graphql',graphqlHTTP({}));
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:true
+}));
 
 app.listen(3000, ()=>{
     console.log("app is listening at port 3000");
--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,14 @@
 const graphql = require("graphql");
 
-const {GraphQLObjectType,GraphQLString} = graphql;
+const {GraphQLObjectType,GraphQLString,GraphQLSchema} = graphql;
+const _ = require("lodash");
+// dummy data
 
+var books =[
+    {name:"avatar1" ,id:"1" ,gener:"sci-fi"},
+    {name:"avatar2" ,id:"2", gener:"fantasy"},
+    {name:"avatar3" ,id:"3" ,gener:"sci-fi"}
+]
 const bookType = new GraphQLObjectType({
     name:"Book",
     fields:()=>({
@@ -9,5 +16,21 @@ const bookType = new GraphQLObjectType({
         name:{type: GraphQLString},
         genre:{type:GraphQLString}
     })
+})
 
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields:{
+        book:{
+            type: bookType,
+            args : {id: { type:GraphQLString}},
+            resolve(parent,args){
+               return  _.find(books,{id:args.id});
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
